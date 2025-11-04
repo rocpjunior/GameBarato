48b2b75f83b64c356f7e255335f3cb1bc31f4851
rocpjunior <jugatenho@hotmail.com>
2025-11-03 23:46:52 -0300

    Implementando opcao de cadastrar endereco e criando a tela de endrereco

--- app/src/main/java/com/example/gamebarato/AdicionarEndereco.kt
+++ app/src/main/java/com/example/gamebarato/AdicionarEndereco.kt
@@ -1,16 +1,72 @@
 package com.example.gamebarato
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class AdicionarEndereco : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_adicionar_endereco)
+        val botaoCadastrarEndereco = findViewById<Button>(R.id.btnCadastrarEndereco)
+        botaoCadastrarEndereco.setOnClickListener(this::salvandoEndereco)
+    }
+
+    protected fun salvandoEndereco(view: View) {
+
+        val edCEP = findViewById<EditText>(R.id.txtCep)
+        val edApelido = findViewById<EditText>(R.id.txtApelido)
+        val edLogradouro = findViewById<EditText>(R.id.txtLogradouro)
+        val edNumero = findViewById<EditText>(R.id.txtNumero)
+        val edComplemento = findViewById<EditText>(R.id.txtComplemento)
+        val edReferencia = findViewById<EditText>(R.id.txtReferencia)
+        val edBairro = findViewById<EditText>(R.id.txtBairro)
+        val edCidade = findViewById<EditText>(R.id.txtCidade)
+        val edUf = findViewById<EditText>(R.id.txtUF)
+
+        var cep = edCEP.text.toString()
+        var apelido = edApelido.text.toString()
+        var logradouro = edLogradouro.text.toString()
+        var numero = edNumero.text.toString()
+        var complemento = edComplemento.text.toString()
+        var referencia = edReferencia.text.toString()
+        var bairro = edBairro.text.toString()
+        var cidade = edCidade.text.toString()
+        var uf = edUf.text.toString()
+
+        val intencao = Intent(this, CadastroPerfil::class.java)
+        intencao.putExtra("CEP", cep)
+        intencao.putExtra("Apelido", apelido)
+        intencao.putExtra("Logradouro", logradouro)
+        intencao.putExtra("Numero", numero)
+        intencao.putExtra("Complemento", complemento)
+        intencao.putExtra("Referencia", referencia)
+        intencao.putExtra("Bairro", bairro)
+        intencao.putExtra("Cidade", cidade)
+        intencao.putExtra("UF", uf)
+        if (cep.isEmpty() && apelido.isEmpty() && logradouro.isEmpty() && numero.isEmpty() && complemento.isEmpty() && referencia.isEmpty() && bairro.isEmpty() && cidade.isEmpty() && uf.isEmpty()) {
+            Toast.makeText(this, "Preencha todos os campos com *", Toast.LENGTH_SHORT).show()
+        } else {
+            //resultado.launch(intencao)
+        }
+
+        val botaoCadastrarEndereco = findViewById<Button>(R.id.btnCadastrarEndereco)
+        botaoCadastrarEndereco.setOnClickListener{
+            val resultadoIntencao = Intent()
+            resultadoIntencao.putExtra("EnderecoCadastrado", """
+                """.trimIndent())
+            setResult(Activity.RESULT_OK, resultadoIntencao)
+            this.finish()
+        }
     }
 }
--- app/src/main/java/com/example/gamebarato/CadastroPerfil.kt
+++ app/src/main/java/com/example/gamebarato/CadastroPerfil.kt
@@ -4,13 +4,48 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.jvm.java
 
 class CadastroPerfil : AppCompatActivity() {
+
+    private var enderecoCadastrado: String = ""
+    protected val resultado = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){resultadoAtividade ->
+        if(resultadoAtividade.resultCode == RESULT_OK){
+            val data: Intent? = resultadoAtividade.data
+            val resultadoFinal = data?.getStringExtra("EnderecoCadastrado")
+            enderecoCadastrado = resultadoFinal!!
+            Toast.makeText(this, enderecoCadastrado, Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro_perfil)
+       //val cep = intent.getStringExtra("CEP")
+
+
+        val botaoCadastrarNovoEnd = findViewById<Button>(R.id.btnCadastrarNovEnd)
+        val botaoCriarCadstro = findViewById<Button>(R.id.btnConfirmaCadastro)
+
+
+        botaoCadastrarNovoEnd.setOnClickListener(this::telaNovoEndereco)
+        botaoCriarCadstro.setOnClickListener(this::telaCadastroCriado)
+    }
+
+    fun telaNovoEndereco (view: View) {
+        val intent = Intent(this, AdicionarEndereco::class.java)
+        startActivity(intent)
+    }
+
+    fun telaCadastroCriado (view: View) {
+        val intent = Intent(this, Login::class.java)
+        finish()
     }
 }
